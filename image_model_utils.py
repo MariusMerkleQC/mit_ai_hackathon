1631ecf3948141f4060e2e9e27c32b19207395c1
Marius Merkle <marius.merkle@quantco.com>
2025-05-03 12:59:54 +0200

    format

--- image_model_utils.py
+++ image_model_utils.py
@@ -7,12 +7,14 @@ from sklearn.neighbors import NearestNeighbors
 import torch
 
 def extract_penultimate_features(image: Image.Image) -> np.ndarray:
-    preprocessor = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
+    preprocessor = transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
 
     model = resnet18()
     model = nn.Sequential(*list(model.children())[:-1])
